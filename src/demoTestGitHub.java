f9c6043eebf8a3a83312e874d46262706ce1104a
azurevn <azurevn@hotmail.com>
2012-11-02 23:39:11 +0700

    Tạo file mới để test
    và thêm test case

---
+++ src/demoTestGitHub.java
@@ -0,0 +1,24 @@
+
+public class demoTestGitHub {
+	set a = new set();
+
+	public demoTestGitHub() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public demoTestGitHub(set a) {
+		super();
+		this.a = a;
+	}
+
+	public set getA() {
+		return a;
+	}
+
+	public void setA(set a) {
+		this.a = a;
+	}
+	
+
+}
